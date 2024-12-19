69a058803c0085e8d263a54ed81c6e20214b24d9
Siddharth Patel <patelsiddharth264@gmail.com>
2024-12-19 20:33:14 +0530

    Add files via upload

--- protfolio.py
+++ protfolio.py
@@ -1,10 +1,8 @@
 import streamlit as st
 c1,c2,c3 = st.columns([1,4,1])
-t1,t2,t3,t4,t5=st.tabs(["Home🏠","Experience💼","Project🎯","Education🎓","Conatact Me📞"])
-#home=st.button("Home")
-#conatct=st.button("Conatact Me")
-
-t1.title("About Me🙋‍♂️")
+t1,t2,t3,t4,t5=st.tabs(["Home🏠","Experience💼","Project🎯","Education🎓","Contact Me📞"])
+#t1.image("P (1).png",width=400)
+t1.title(":orange[About Me🙋‍♂️]")
 b1,b2=t1.columns([3,1])
 b1.write('''I am a passionate and dedicated Bachelor of Computer Applications (BCA) student with a strong
             foundation in computer science, programming, and software development. I thrive on learning and
@@ -15,3 +13,37 @@ b1.write('''A motivated and skilled Bachelor of Computer Applications graduate w
 foundation in software development, programming languages, and emerging
 technologies. Passionate about creating innovative technological solutions and 
 continuously expanding technical expertise through hands-on projects and learning.''')
+t2.title(":orange[Experience💼]")
+t2.write('''As a passionate BCA student, I have developed a strong foundation in computer applications,
+         programming, and problem-solving. While I do not have formal work experience, I have actively
+         engaged in academic projects, coursework, and personal initiatives that showcase my technical and
+         collaborative skills.''')
+t3.header(":orange[Project🎯]")
+t3.write('''As a BCA (Bachelor of Computer Applications) student, I am in the process of building a strong
+         foundation in programming, database management, and software development. While I do not
+         yet have formal projects to showcase, my coursework and personal learning initiatives have
+         equipped me with the skills and knowledge to undertake meaningful projects in the future.''')
+t3.header(":orange[Future Project Aspirations🔮]")
+t3.write('''I am eager to apply my skills to real-world projects and am actively exploring opportunities to:
+           ➡️Develop applications that solve everyday problems,
+          Work on collaborative team projects to gain practical experience in software development.
+         Build dynamic websites and mobile apps using advanced technologies.''')
+t4.title(":orange[Education🧑🏻‍🎓]")
+t4.header(":green[Bachelor of Computer Applications (BCA)💻]")
+t4.write('''Developed a strong foundation in programming, database management, software development, and web technologies.
+             Studied core subjects including Data Structures, Operating Systems, Networking, Software Engineering, and Object-Oriented Programming.
+             Participated in workshops, seminars, and tech events to enhance technical and professional knowledge.''')
+t4.header(":green[Achievements🏅]")
+t4.write(''':red[Second Runner-Up – Internal Hackathon]
+            Recognized as part of the second runner-up team in an internal college hackathon.
+         This experience honed my skills in teamwork, critical thinking, and quick problem-solving under pressure.
+         ''')
+t4.link_button("👨‍💻Go to Hackathon Gallery👨‍💻","https://photos.app.goo.gl/EXJaznETNTwaJ5NJ6")
+t5.header(":orange[Contact ☎️]")
+t5.write('''Whether you have a question, a project collaboration, or a professional opportunity, feel free to reach out to me.''')
+t5.subheader(":red[Get in Touch👆🏼]")
+t5.info("Contact me any time")
+#t5.selectbox("Choose Option",["Linkedin","Email","Phone No.",])
+t5.page_link("https://www.linkedin.com/in/siddharth-patel-108581304/", label="💻Linkedin")
+t5.write("📧patelsiddharth264@gmail.com")
+t5.write("📱7084899321")
